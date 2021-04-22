86777fee8ca547875d8e9d7d4ed492d93f14c96e
michaelats <74707662+michaelats@users.noreply.github.com>
2021-04-22 23:46:53 +0700

    Update about_us.blade.php

--- resources/views/about_us.blade.php
+++ resources/views/about_us.blade.php
@@ -9,37 +9,100 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <title>First Rental Mobil</title>
+    <style>
+        img {
+            max-width: 100%;
+            height: auto;
+        }
+        *{
+            margin: 0;
+            padding: 0;
+            font-family: "Open Sans",sans-serif;
+            box-sizing: border-box;
+        }
+
+        body{
+            min-height: 100vh;
+            align-items: center;
+            justify-content: center;
+            background-color: #f1f1f1;
+        }
+
+        .about-section{
+            background-size: 55%;
+            background-color: #fdfdfd;
+            overflow: hidden;
+            padding: 100px 0;
+        }
+
+        .inner-container{
+            width: 55%;
+            float: right;
+            background-color: #fdfdfd;
+            padding: 150px;
+        }
+
+        .inner-container h1{
+            margin-bottom: 30px;
+            font-size: 30px;
+            font-weight: 900;
+        }
+
+        .text{
+            font-size: 13px;
+            color: #545454;
+            line-height: 30px;
+            text-align: justify;
+            margin-bottom: 40px;
+        }
+
+        .sosialmedia{
+            display: flex;
+            justify-content: space-between;
+            font-weight: 700;
+            font-size: 13px;
+        }
+    </style>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">First Rental Mobil</a>
-  <div class="container-fluid">
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/car">Car</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/rent" tabindex="-1" aria-disabled="true">Rent</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/about us" tabindex="-1" aria-disabled="true">About Us</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/keluar" tabindex="-1" aria-disabled="true">Log Out</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+    <nav class="navbar navbar-expand-lg navbar-light bg-primary mb-3">
+      <a class="navbar-brand">First Rental Mobil</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/car">Car</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="/rent" tabindex="-1" aria-disabled="true">Rent</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about us" tabindex="-1" aria-disabled="true">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/keluar" tabindex="-1" aria-disabled="true">Log Out</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+<img src="aboutuscar.jpeg">
+    <div class="inner-container">
+        <h1>About Us</h1>
+        <p class="text">
+        First Rental Adalah Perusahaan Rental Mobil Dengan Harga Terjangakau Dan Armada Baru Yang Siap Mengantarkan Perjalanan Anda Menjadi Lebih Nyaman Dan Berkesan. Kami Berkomitmen Untuk Menyajikan Pelayanan Terbaik Dengan Menyiapkan Kendaraan Yang Bisa Diandalkan Baik Dari Aspek Performa Maupun Estetika. Didukung Oleh Staf Pelayanan Pelanggan Yang Siap Menjawab Pertanyaan Dan Memberikan Solusi Terbaik Untuk Anda. 
+        </p>
+        <div class="sosialmedia">
+            <p>Instagram<br>@firstrental</p><br>
+            <p>WhatsApp<br>082167671029</p><br>
+            <p>Location<br>Jln.GatotSubroto No 120,Medan</p><br>
+        </div>
     </div>
-  </div>
-</nav>
-
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
