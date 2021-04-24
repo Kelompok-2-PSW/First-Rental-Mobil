03f54bf2ca81bd732f81d7eb7a53954bdced615f
Ziva22 <82991869+Ziva22@users.noreply.github.com>
2021-04-23 22:02:43 -0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,68 +1,128 @@
-@extends('layout/main')
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('title', 'First Rental Mobil')
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" 
+    crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Arvo&family=Clepster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rochester&display=swap" rel="stylesheet"> 
+    <title> First Rental Mobil</title>
+    <style>
+    body{
+      background-color: rgb(161, 167, 160);
 
-@section('container')
+      font-size:35px;
+    }
+    #judul{
+      font-family:  Copperplate, Papyrus, fantasy;
+      font-size:50px;
+    }
+    #judul1{
+      font-size:80px;
+      font-family: 'Arvo', serif;
+      text-shadow: 0px 0px 20px #232323;
+      color:rgb(73, 209, 209);
+      font-style: bold;
+    }
+
+    #judul2{
+      font-size:50px;
+      font-family: 'Josefin Slab', serif;
+    }
+
+    .isi{
+      font-family: 'Oranienbaum', serif;
+      font-size: 40px;
+      text-align: justify;
+    }
+
+    #footer{
+      background-color:  rgb(153, 152, 156);
+    }
+    </style>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-dark mb-3 text-white">
+  <h2 id="judul">First Rental Mobil</h2>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end text-white" id="navbarNavAltMarkup">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link active tetxt" aria-current="page" href="/" id="select">Home |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/car">Car |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about us" tabindex="-1" aria-disabled="true">About Us |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/rent" tabindex="-1" aria-disabled="true">Login |</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/keluar" tabindex="-1" aria-disabled="true">Log Out </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
 <div class="container">
-    <div class="row">
-        <div class="col">
-        <marquee>
-        <h4 class="color:grey">Selamat Datang di First Rental Mobil Kami</h4>
+        <marquee scrolldelay="50" bgcolor="white">
+        <h4 id="judul2" >Selamat Datang di First Rental Mobil Kami</h4>
         </marquee><br><br>
-        <h1 class="text-center" class="mt-4" id>First Rental Mobil<h1>
-        <img src="image/gbr1.jpg" width="1130px" height="700px">
-        <h2><small>
-            First Rental Mobil merupakan sebuah aplikasi berbasis web mengenai sewa mobil.<br>
-            Dengan menggunakan aplikasi berbasis web ini, Anda dapat dengan mudah melakukan pemesanan maupun melihat
-            mengenai informasi rental mobil ini dan tidak perlu datang ke tempat.<br>
 
-        </h2>
-        </p>
-        </div>
-    </div>
+        <h1 class="text-center" id="judul1">FIRST RENTAL MOBIL<h1><br>
+        <img src="image/gbr2.jpg" width="1300px" height="700px"><br><br>
+        <h1 class="isi">
+            First Rental Mobil merupakan sebuah aplikasi berbasis web mengenai sewa mobil.
+            Dengan menggunakan aplikasi berbasis web ini, Anda dapat dengan mudah dan 
+            lebih efektif untuk melakukan pemesanan maupun melihat
+            mengenai informasi rental mobil ini dan tidak perlu datang ke tempat.<br>
+        </h1>
 </div><br>
-<footer class="bg-light text-center text-lg-start">
-  <!-- Grid container -->
+<footer class="text-center text-lg-start text-white" style="background-color:rgb(68, 71, 67);">
   <div class="container p-4">
     <!--Grid row-->
     <div class="row">
-      <!--Grid column-->
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Keuntungan mengenai Harga Sewa</h5>
+        <h1 class="isi1">Keuntungan mengenai Harga Sewa</h1>
 
-        <p>
+        <p class="isi">
         Dalam menggunakan aplikasi berbasis web ini, sangatlah mudah dan terdapat diskon maupun potongan harga
         </p>
       </div>
-      <!--Grid column-->
-
-      <!--Grid column-->
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Keuntungan mengenai Waktu Operasi</h5>
-
-        <p>
-          Keuntungan dalam menggunakan aplikasi berbasis web ini, yaitu waktu operasi sangat efektif dibandingkan dengan langsung ke lokasi langsung
-        </p>
-      </div>
 
       <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h5 class="text-uppercase">Keuntungan mengenai Keamanan Sewa</h5>
+        <h1 class="isi1">Keuntungan mengenai Waktu Operasi</h1>
+        <p class="isi">
+        Keuntungan dalam menggunakan aplikasi berbasis web ini, 
+        yaitu waktu operasi sangat efektif dibandingkan dengan langsung ke lokasi langsung
+        </p>
+      </div><br>
 
-        <p>
-          Keuntungan dalam menggunakan aplikasi berbasis web ini, yaitu keamanan sewa yang sangat terjangkau
+      <center>
+      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+        <h1 class="isi1">Keuntungan mengenai Keamanan Sewa</h1>
+        <p class="isi">
+        Keuntungan dalam menggunakan aplikasi berbasis web ini, yaitu keamanan sewa yang sangat terjangkau
         </p>
       </div>
-      <!--Grid column-->
+     </center>
     </div>
-    <!--Grid row-->
   </div>
-  <!-- Grid container -->
-
-  <!-- Copyright -->
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2021 By:
-    <a class="text-dark">First_Rental_Mobil.com</a>
+  <div class="text-center p-3" style="background-color:rgb(24, 27, 27);">
+    © 2020 By :
+    <a>First Rental Mobil</a>
   </div>
-  <!-- Copyright -->
 </footer>
-@endsection
+</body>
+</html>
