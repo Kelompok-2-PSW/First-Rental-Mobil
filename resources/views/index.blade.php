75441ca41960bec3df4cc6c7c077b187a6775a4d
Ziva22 <82991869+Ziva22@users.noreply.github.com>
2021-04-25 06:43:14 -0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,32 +1,10 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout/main')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" 
-    rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" 
-    crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Slab&display=swap" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css2?family=Arvo&family=Clepster&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Rochester&display=swap" rel="stylesheet"> 
-    <title> First Rental Mobil</title>
-    <style>
-    body{
-      background-color: rgb(161, 167, 160);
-
-      font-size:35px;
-    }
-    #judul{
-      font-family:  Copperplate, Papyrus, fantasy;
-      font-size:50px;
-    }
+@section('judul', 'First Rental')
+<style>
     #judul1{
       font-size:80px;
-      font-family: 'Arvo', serif;
+      font-family: 'serif';
       text-shadow: 0px 0px 20px #232323;
       color:rgb(73, 209, 209);
       font-style: bold;
@@ -34,52 +12,21 @@
 
     #judul2{
       font-size:50px;
-      font-family: 'Josefin Slab', serif;
+      font-family: 'serif';
     }
 
     .isi{
       font-family: 'Oranienbaum', serif;
-      font-size: 40px;
+      font-size: 20px;
       text-align: justify;
     }
-
-    #footer{
-      background-color:  rgb(153, 152, 156);
-    }
     </style>
-  </head>
-  <body>
-  <nav class="navbar navbar-expand-lg bg-dark mb-3 text-white">
-  <h2 id="judul">First Rental Mobil</h2>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse justify-content-end text-white" id="navbarNavAltMarkup">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active tetxt" aria-current="page" href="/" id="select">Home |</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/car">Car |</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/about us" tabindex="-1" aria-disabled="true">About Us |</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/rent" tabindex="-1" aria-disabled="true">Login |</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/keluar" tabindex="-1" aria-disabled="true">Log Out </a>
-      </li>
-    </ul>
-  </div>
-</nav>
+@section('container')
 
-<div class="container">
         <marquee scrolldelay="50" bgcolor="white">
         <h4 id="judul2" >Selamat Datang di First Rental Mobil Kami</h4>
         </marquee><br><br>
-
+        <div class="container">
         <h1 class="text-center" id="judul1">FIRST RENTAL MOBIL<h1><br>
         <img src="image/gbr2.jpg" width="1300px" height="700px"><br><br>
         <h1 class="isi">
@@ -93,7 +40,7 @@
   <div class="container p-4">
     <!--Grid row-->
     <div class="row">
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+      <div class="col-lg-8 col-md-4 mb-4 mb-md-0">
         <h1 class="isi1">Keuntungan mengenai Harga Sewa</h1>
 
         <p class="isi">
@@ -101,7 +48,7 @@
         </p>
       </div>
 
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+      <div class="col-lg-8 col-md-4 mb-4 mb-md-0" >
         <h1 class="isi1">Keuntungan mengenai Waktu Operasi</h1>
         <p class="isi">
         Keuntungan dalam menggunakan aplikasi berbasis web ini, 
@@ -109,20 +56,16 @@
         </p>
       </div><br>
 
-      <center>
-      <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+      <div class="col-lg-8 col-md-4 mb-4 mb-md-0">
         <h1 class="isi1">Keuntungan mengenai Keamanan Sewa</h1>
         <p class="isi">
         Keuntungan dalam menggunakan aplikasi berbasis web ini, yaitu keamanan sewa yang sangat terjangkau
         </p>
       </div>
-     </center>
+
     </div>
-  </div>
-  <div class="text-center p-3" style="background-color:rgb(24, 27, 27);">
-    © 2020 By :
-    <a>First Rental Mobil</a>
   </div>
 </footer>
 </body>
 </html>
+@endsection
