ca132ad1b569ee3922c5578b122c1748649d94b7
Ziva22 <82991869+Ziva22@users.noreply.github.com>
2021-05-15 05:01:05 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\WelcomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\crudController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,16 +18,47 @@ use App\http\Controllers\WelcomeController;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('login', 'App\Http\Controllers\AuthController@index')->name('login');
+Route::get('register', 'App\Http\Controllers\AuthController@register')->name('register');
+Route::post('simpanregister', 'App\Http\Controllers\AuthController@simpanregister')->name('simpanregister');
+Route::post('proses_login', 'App\Http\Controllers\AuthController@proses_login')->name('proses_login');
+Route::get('logout', 'App\Http\Controllers\AuthController@logout')->name('logout');
 
-Route::get('/about', 'PagesController@about');
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['cek_login:admin']], function () {
+        Route::resource('admin', AdminController::class);
+    });
+    Route::group(['middleware' => ['cek_login:customer']], function () {
+        Route::resource('customer', CustomerController::class);
+    });
 
-Route::get('/car', 'CarController@index');
+    Route::resource('crud', crudController::class);
+    Route::get('/crud/delete/{id}', [crudController::class, 'destroy'])->name('crud.destroy');
+    Route::post('/crud/update/{id}', [crudController::class, 'update'])->name('crud.update');
+});
 
-Route::get('/rent', 'PagesController@rent');
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
 
-Auth::routes();
+Route::get('/customer', function () {
+    return view('customer.dashboard');
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/car', 'App\Http\Controllers\CarController@index')->name('car.index');
+Route::get('/crud', 'App\Http\Controllers\crudController@index')->name('admin.crud.index');
+Route::get('/about', 'App\Http\Controllers\CustomerController@about');
 
+Route::get('/rent', 'App\Http\Controllers\CustomerController@rent');
+
+Route::get('/tambah', function () {
+    return view('inputdata');
+    });
+    
+Route::post('/tambahdata', 'App\Http\Controllers\RentController@tambah')->name('tambahdata');
+
+Route::get('/lihatdata', 'App\Http\Controllers\LaporanController@index')->name('admin.lihatdata');
