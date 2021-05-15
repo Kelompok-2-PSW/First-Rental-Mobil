0e2ba0172cf6e579c307bdbe895fecdfd86adeb6
Ziva22 <82991869+Ziva22@users.noreply.github.com>
2021-05-15 04:52:06 -0700

    Add files via upload

---
+++ database/migrations/2021_05_04_155547_create_mobil_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMobilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mobil', function (Blueprint $table) {
+            $table->id('id_mobil');
+            $table->string('no_mobil');
+            $table->string('merk');
+            $table->string('gambar');
+            $table->integer('harga');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mobil');
+    }
+}
---
+++ database/migrations/2021_05_13_225634_create_rent_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rent', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pemesanan');
+            $table->string('kode_mobil');
+            $table->string('nama_mobil');
+            $table->string('tanggal_pemesanan');
+            $table->string('alamat');
+            $table->string('no_telephone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rent');
+    }
+}
